e2db3650b1f5ddc1aa776cb0d577ebdffa2c72cf
Jay <jordan.keezell@gmail.com>
2019-10-31 13:18:50 -0700

    created ftoc function and ctof

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+def ftoc(number)
+#farenheit to celsius formula
+c = (number.to_f - 32.0)*(5.0/9.0)
+return c
+end
+def ctof(arg)
+#celsius to farenheit formula
+    f = (9.0 / 5.0 * arg) + 32.0
+return f
+end
