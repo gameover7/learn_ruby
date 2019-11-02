3b88ab1e7ef83c8e7e988fcc9e3b2fb0db075b44
Jay <jordan.keezell@gmail.com>
2019-11-01 20:33:15 -0700

    updated frustrating project

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+
+def echo(arg)
+return arg
+end
+
+def shout(arg)
+return arg.upcase
+end
+
+def repeat(arg, num_of_reps = 2)
+return ((arg + ' ') * num_of_reps).delete_suffix(' ') 
+end
+
+def start_of_word(arg, num_of_letters = 1 )
+    
+    return arg[0..num_of_letters - 1]
+end
+
+def first_word(sentence)
+sentence.split(' ')[0]
+end
+
+def titleize(sentence)
+    smallWords = ['and', 'the', 'over']
+result = sentence.split
+result = result.each do |word|
+    if(smallWords.include? word || word.length < 3) && word != result[0]
+    result = word
+    else
+    result = word.capitalize!
+    end
+end
+return result.join(' ')
+end
