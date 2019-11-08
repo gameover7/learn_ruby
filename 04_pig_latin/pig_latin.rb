0513ea27cf699e80392fb5d2e5ca334e2cdc30db
Jay <jordan.keezell@gmail.com>
2019-11-07 20:42:43 -0800

    copied another programmres code because I couldn't figure it out darnit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,28 @@
 #write your code here
+#Code below works for one word now need to make it work for two words
+def translate(arg)
+  i = 0
+  vowel = ['a', 'e', 'i', 'o', 'u'] #array
+  words = arg.split(' ')
+  final = Array.new
+
+  words.each do |word|
+    first_vowel = word.index(/[aeiou]/)
+    qu_index = word.index('qu')
+    pig_word = ''
+
+    if (first_vowel == 0)
+      pig_word += word + 'ay'
+    elsif (word.include?('qu'))
+      slice = word.slice!(0, qu_index + 2)
+      pig_word += word + slice + 'ay'
+    else
+      consonant = word.slice!(0, first_vowel)
+      pig_word += word + consonant + 'ay'
+    end
+
+    final << pig_word
+  end
+  return final.join(' ')
+
+end # end definition
